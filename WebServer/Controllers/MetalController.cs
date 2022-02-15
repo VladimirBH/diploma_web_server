5821b17405bfc14f1ca6c55c8938094c32a26993
VladimirBH <vladimirburbah07@gmail.com>
2022-02-15 17:41:37 +0300

    Добавил контроллер, создал класс для записи в него состава металаа

---
+++ WebServer/Controllers/MetalController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebServer.Classes;
+
+namespace WebServer.Controllers
+{
+    [Produces("application/json")]
+    public class MetalController : Controller
+    {
+        // GET: MetalController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MetalController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MetalController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MetalController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+
+            try
+            {
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MetalController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MetalController/Structures
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Structures(MetalStructure structure)
+        {
+            try
+            {
+                
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MetalController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MetalController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebServer/MetalStructure.cs
@@ -0,0 +1,31 @@
+﻿namespace WebServer.Classes
+{
+    public class MetalStructure
+    {
+        public int furnace { get; set; }
+
+        public double ag { get; set; }
+
+        public double al { get; set; }
+
+        public double au { get; set; }
+
+        public double ca { get; set; }
+
+        public double cr { get; set; }
+
+        public double cu { get; set; }
+
+        public double fe { get; set; }
+
+        public double ni { get; set; }
+
+        public double pb { get; set; }
+
+        public double si { get; set; }
+
+        public double sn { get; set; }
+
+        public double zn { get; set; }
+    }
+}

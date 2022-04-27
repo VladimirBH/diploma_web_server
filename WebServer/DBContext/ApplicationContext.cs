e784de015a4ee95f81fec505d96edcf425a537cb
VladimirBH <vladimirburbah07@gmail.com>
2022-04-27 23:47:14 +0300

    Накинул DBContext, добавил в nuget JWT и EntityFramework для Postgres

---
+++ WebServer/DBContext/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebServer.DBContext
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=usersdb;Username=postgres;Password=здесь_указывается_пароль_от_postgres");
+        }
+    }
+}
